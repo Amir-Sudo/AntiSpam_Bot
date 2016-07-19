fe2e558399e9ee246d429b910203de14e0b1a4d5
amirhossein99 <amousavi316@gmail.com>
2016-07-20 03:25:18 +0430

    Create spam.lua

---
+++ plugins/spam.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+       if not is_sudo(msg) then
+              return "Only Sudoers Can Use it"       end
+  local tex = matches[1]
+  local sps = matches[2]
+  local sp = 'Sucessfully done'
+
+for i=1, tex, 1 do
+
+sp = '\n\n'..sps..'\n\n'..sp
+i = i + 1
+
+end
+
+return sp
+
+end
+
+return {
+    patterns = {
+      "spam (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
+--by @amir_anonymous
+--channel @powerspeed_ch
