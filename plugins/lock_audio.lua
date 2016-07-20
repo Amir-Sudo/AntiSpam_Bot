b1f75e85089da50352b2738c4a0961f24cf25c50
amirhossein99 <amousavi316@gmail.com>
2016-07-21 00:32:10 +0430

    Add files via upload

---
+++ plugins/lock_audio.lua
@@ -0,0 +1,33 @@
+--An empty table for solving multiple kicking problem(thanks to @topkecleon )
+kicktable = {}
+local function run(msg, matches)
+if msg.to.type == 'channel' then
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_audio'] then
+                lock_audio = data[tostring(msg.to.id)]['settings']['lock_audio']
+            end
+        end
+    end
+    local channel = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_audio == "yes" then
+        send_large_msg(channel, '')
+        delete_msg(msg.id, ok_cb, false)
+    end
+end
+ end
+return {
+ usage = {
+  "lock tag: If User Send A Message With # , @ Then Bot Removed User.",
+  "unlock tag: No Action Execute If User Send Mesage With # , @",
+  },
+  patterns = {
+    "(%[(audio)%])",
+ },
+  run = run
+}
